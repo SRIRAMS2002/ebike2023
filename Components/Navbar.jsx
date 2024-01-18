2ede58965a7c979dc65fb01895716e124db774e4
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2024-01-18 23:26:15 +0530

    navbar-1

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                Email Address
               </label>
               <div className="mt-2">
                 <input
